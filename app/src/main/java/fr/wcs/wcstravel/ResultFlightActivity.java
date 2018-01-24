2f625ab39e475a9ec9245565da8bd69560be6c25
quent176 <quentin7611@yahoo.fr>
2018-01-24 14:48:37 +0100

    add result activity

--- app/src/main/java/fr/wcs/wcstravel/ResultFlightActivity.java
+++ app/src/main/java/fr/wcs/wcstravel/ResultFlightActivity.java
@@ -2,6 +2,7 @@ package fr.wcs.wcstravel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Locale;
@@ -9,11 +10,17 @@ import java.util.Locale;
 public class ResultFlightActivity extends AppCompatActivity {
 
     TravelModel mTravelModel;
+    TextView mResultDeparturePlace, mResultArrivalPlace, mResultDepartureDate, mResultReturnDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_flight);
+
+        mResultDeparturePlace = findViewById(R.id.result_departure_place);
+        mResultArrivalPlace = findViewById(R.id.result_arrival_place);
+        mResultDepartureDate = findViewById(R.id.result_departure_date);
+        mResultReturnDate = findViewById(R.id.result_return_date);
 
         //récupération de l'objet via Intent
         mTravelModel = getIntent().getExtras().getParcelable("TheTravel");
@@ -21,9 +28,9 @@ public class ResultFlightActivity extends AppCompatActivity {
         String myFormat = "yyyy-MM-dd";
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
 
-       // _editText.setText(sdf.format(mTravelModel.getDepartureDate().getTime()));
-         System.out.println(sdf.format(mTravelModel.getDepartureDate().getTime()));
-      //  _editText.setText(sdf.format(mTravelModel.getReturnDate().getTime()));
-         System.out.println(sdf.format(mTravelModel.getReturnDate().getTime()));
+        mResultDeparturePlace.setText(mTravelModel.getDeparturePlace());
+        mResultArrivalPlace.setText(mTravelModel.getArrivalPlace());
+        mResultDepartureDate.setText(sdf.format(mTravelModel.getDepartureDate().getTime()));
+        mResultReturnDate.setText(sdf.format(mTravelModel.getReturnDate().getTime()));
     }
 }
