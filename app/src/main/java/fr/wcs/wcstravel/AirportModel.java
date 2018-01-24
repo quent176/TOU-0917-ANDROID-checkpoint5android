5bab4d60efd7fca145afbc7f6626f3c1fc7bb1b7
quent176 <quentin7611@yahoo.fr>
2018-01-24 14:02:50 +0100

    add autosuggest

---
+++ app/src/main/java/fr/wcs/wcstravel/AirportModel.java
@@ -0,0 +1,36 @@
+package fr.wcs.wcstravel;
+
+/**
+ * Created by apprenti on 24/01/18.
+ */
+
+public class AirportModel {
+
+    private String label;
+    private String value;
+
+    public AirportModel(String label, String value) {
+        this.label = label;
+        this.value = value;
+    }
+
+    public AirportModel(){
+        //for Firebase
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
--- app/src/main/java/fr/wcs/wcstravel/SearchFlightActivity.java
+++ app/src/main/java/fr/wcs/wcstravel/SearchFlightActivity.java
@@ -1,34 +1,65 @@
 package fr.wcs.wcstravel;
 
-import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Locale;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class SearchFlightActivity extends AppCompatActivity {
 
-    EditText mEditDepartureDate, mEditReturnDate, mEditDeparturePlace, mEditArrivalPlace;
+    private EditText mEditDepartureDate, mEditReturnDate;
+    private AutoCompleteTextView mEditDeparturePlace, mEditArrivalPlace;
+    private DatabaseReference mAirportDB;
+
+    private ArrayAdapter<String> airportAdapter = null;
+    private AutoCompleteTextView autoCompleteSearchUsers;
+    private ArrayList<String> listAirport = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_flight);
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        mAirportDB = database.getReference("checkpoint5/airports");
+
         mEditDeparturePlace = findViewById(R.id.edit_departure_place);
         mEditArrivalPlace = findViewById(R.id.edit_arrival_place);
         mEditDepartureDate = findViewById(R.id.edit_departure_date);
         mEditReturnDate = findViewById(R.id.edit_return_date);
         Button buttonSearch = (Button) findViewById(R.id.button_search_trip);
+
+        //Auto Suggestion Friends from Database
+        mAirportDB.orderByChild("value").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                listAirport.clear();
+                for (DataSnapshot data : dataSnapshot.getChildren()) {
+                    AirportModel airportModel = data.getValue(AirportModel.class);
+                    listAirport.add(airportModel.getLabel());
+                }
+                airportAdapter = new ArrayAdapter<String>(SearchFlightActivity.this, R.layout.search_box, R.id.tvHintCompletion, listAirport);
+                mEditDeparturePlace.setAdapter(airportAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         //Ajout du datepicker pour la date
         final MyEditTextDatePicker datePicker = new MyEditTextDatePicker(this, R.id.edit_departure_date);
